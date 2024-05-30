a35823fdc33299c0c419cc0e8634b43750dd8366
Giovani <jiovanisantos9@gmail.com>
2024-05-30 17:50:21 -0100

    add remove-task

--- task.py
+++ task.py
@@ -6,28 +6,30 @@ tasks = []
 def add_task(task):
     tasks.append({"task": task, "completed": False})
 
-    while True:
-        command = input("Comando (add/exit): ")
-        if command == "add":
-            task = input("Tarefa: ")
-            add_task(task)
-        elif command == "exit":
-            break
-
 def list_tasks():
     for i, task in enumerate(tasks):
         status = "Concluída" if task["completed"] else "Não Concluída"
-        print(f"{i + 1}. {task['task']} [{status}]")
+        print(f"{i + 1}. {task['task']} [{status}]")   
+
+def remove_task(index):
+    if 0 >= index < len(tasks):
+        tasks.pop(index)
+    else:
+        print("Índice inválido")         
 
     while True:
-        command = input("Comando (add/list/exit): ")
+        command = input("Comando (add/list/remove/exit): ")
         if command == "add":
             task = input("Tarefa: ")
             add_task(task)
         elif command == "list":
             list_tasks()
+        elif command == "remove":
+            index = int(input("Índice da tarefa a remover: ")) - 1
+            remove_task(index)
         elif command == "exit":
             break
+
 
 if __name__ == "__main__":
     main()
